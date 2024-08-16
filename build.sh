187df262c6ba3da9403bb9b87ebb591ac64c55b8
jokerwyt <914554688@qq.com>
2024-08-15 23:41:07 -0600

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e 
+
 RELEASE=release-1.22-latest
 
 ENVOY_ROOT=$(dirname $(realpath -s $0))
@@ -12,6 +14,9 @@ echo "APPNET_ROOT: ${APPNET_ROOT}"
 
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
+
+# remove the old envoy, ignore the error if it does not exist
+rm -f ${ENVOY_ROOT}/envoy || true
 
 # echo the command
 echo "docker run -it -w /work -v ${ENVOY_ROOT}:/work --user ${USER_ID}:${GROUP_ID} gcr.io/istio-testing/build-tools-proxy:${RELEASE} /bin/bash -c \"git config --global --add safe.directory /work && make build_envoy && cp /work/bazel-bin/envoy /work/envoy\""
